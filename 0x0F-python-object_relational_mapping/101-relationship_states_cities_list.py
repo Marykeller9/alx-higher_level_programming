8997d0b169b21ca2517e3490cddae3f2a23c29b5
Marykeller9 <marykeller9@gmail.com>
2023-09-18 16:22:14 -0700

    final update

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,30 +1,22 @@
 #!/usr/bin/python3
-"""Module that lists all State objects, and corresponding City objects,\
-        contained in the mySQL database"""
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from relationship_state import State
+from relationship_state import Base, State
 from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
 
 if __name__ == "__main__":
-    # Create a database engine using the provided arguments
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
-    # Create a session factory bound to the engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
-
-    # Create a session object
     session = Session()
-
-    # Retrieve all states from the database and order them by ID
-    for state in session.query(State).order_by(State.id):
-        # Print state ID and name
-        print("{}: {}".format(state.id, state.name))
-
-        # Iterate over the cities associated with the current state
-        for city in state.cities:
-            # Print city ID and name
-            print("    {}: {}".format(city.id, city.name))
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
+        for city_ins in instance.cities:
+            print("    ", end="")
+            print(city_ins.id, city_ins.name, sep=": ")
